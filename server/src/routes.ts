45f5a7c28bac44a5661224ac32a37c35e09023f3
Lucas Viga <lucasviga12@gmail.com>
2022-05-09 00:34:50 -0400

    add try/catch

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,21 +6,27 @@ import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 export const routes = Router();
 
 routes.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body
+  try {
+    const { type, comment, screenshot } = req.body
 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-  const nodemailerMailAdapter = new NodemailerMailAdapter();
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository,
-    nodemailerMailAdapter
-  );
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter
+    );
 
-  await submitFeedbackUseCase.execute({
-    type, 
-    comment, 
-    screenshot
-  })
+    await submitFeedbackUseCase.execute({
+      type, 
+      comment, 
+      screenshot
+    })
 
-  return res.status(201).send();
+    return res.status(201).send();
+  } catch (error) {
+    console.log(error)
+    
+    return res.status(500).send();
+  }
 })
